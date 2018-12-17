dcc27caf7e95116bb3aa78fb6a291deda503a557
Arun Antony <arun.antony@knab.nl>
2018-12-17 13:11:05 +0100

    weather-forcast service: include the api call to get real data instead of hardcoded one

--- src/app/services/weather-forecast/weather-forecast-service.ts
+++ src/app/services/weather-forecast/weather-forecast-service.ts
@@ -18,11 +18,22 @@ export class WeatherForecastService {
     
     constructor () {
         this.weatherForcastPromise = new Promise((resolve, reject) => {
-            const initialForcast = [
-                {cityName : 'Sydney', temperatureNow: 22, forCast: [], selected: false},
-                {cityName : 'Amsterdam', temperatureNow: 2, forCast: [], selected: false},
-            ];
-            resolve(initialForcast);
+            fetch(
+                    'https://nx3zxr5sc4.execute-api.us-east-2.amazonaws.com/test/test-db-connection',
+                    {
+                        method: 'GET',
+                    }
+                )
+                .then((res) => res.json())
+                .then((jsonRes) =>  {
+                    // Ex: [{cityName : 'Sydney', temperatureNow: 22, forCast: [], selected: false}]
+                    const initialForcast = Array.isArray(jsonRes) && 
+                    jsonRes.map((item) => ({...item, forCast: [], selected: false}));
+                    resolve(initialForcast);
+                }).catch((err) => {
+                    // todo: properly handled using a common log service
+                    console.log(err);
+                });
         });
         
         this.weatherForcastPromise.then((data: CityWeather[]) => {
@@ -35,17 +46,30 @@ export class WeatherForecastService {
     }
 
     getCityWeatherForNext5Days (name: string) {
-        
-        const ind = this.weatherForcast.findIndex((weather) => (weather.cityName === name));
+        fetch(
+                'https://nx3zxr5sc4.execute-api.us-east-2.amazonaws.com/test/test-db-connection',
+                {
+                    method: 'POST',
+                    body: JSON.stringify({city: name})
+                }
+            )
+            .then((res) => res.json())
+            .then((jsonRes) =>  {
+                const ind = this.weatherForcast.findIndex((weather) => (weather.cityName === name));
 
-        if (ind !== -1) {
-            const selectedIndx = this.weatherForcast.findIndex((weather) => (weather.selected === true));
-            if (selectedIndx !== -1) {
-                this.weatherForcast[selectedIndx].selected = false;
-            }
-            this.weatherForcast[ind].selected = true;
-            this.weatherForcast[ind].forCast = [12, 7, -5, 0];;
-            this.citySelected.next();
-        }
+                if (ind !== -1) {
+                    const selectedIndx = this.weatherForcast.findIndex((weather) => (weather.selected === true));
+                    if (selectedIndx !== -1) {
+                        this.weatherForcast[selectedIndx].selected = false;
+                    }
+                    this.weatherForcast[ind].selected = true;
+                    // Ex: [12, 7, -5, 0];
+                    this.weatherForcast[ind].forCast = Array.isArray(jsonRes) && jsonRes;
+                    this.citySelected.next();
+                }
+            }).catch((err) => {
+                // todo: properly handled using a common log service
+                console.log(err);
+            });
     }
 }
